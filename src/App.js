5ebfe751cdbd0b9ee2e0b9935bd146732cc199bb
BrxndyM <77096049+BrxndyM@users.noreply.github.com>
2023-04-20 16:34:43 +0200

    updated initial values

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 import BarChart from './Barchart';
 const data = [
   { day: 'Monday', percentage: 50 },
-  { day: 'Tuesday', percentage: 25 },
+  { day: 'Tuesday', percentage: 27 },
   { day: 'Wednesday', percentage: 75 },
   { day: 'Thursday', percentage: 40 },
-  { day: 'Friday', percentage: 80 }
+  { day: 'Friday', percentage: 95 }
 ];
 
 function App() {
